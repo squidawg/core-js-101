83c8c476f632b81249b1e3c812e596cf2e1325f6
squidawg <78506807+squidawg@users.noreply.github.com>
2023-01-17 18:55:46 +0400

    feat: complete 06-promises-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,17 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  let temp = '';
+  if (isPositiveAnswer === true) {
+    temp = Promise.resolve('Hooray!!! She said "Yes"!');
+  } if (isPositiveAnswer === false) {
+    temp = Promise.resolve('Oh no, she said "No".');
+  } if (typeof isPositiveAnswer !== 'boolean') {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    temp = Promise.reject(Error('Wrong parameter is passed! Ask her again.'));
+  }
+  return temp;
 }
 
 
@@ -48,8 +57,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array).then((val) => Promise.resolve(val));
 }
 
 /**
@@ -71,8 +80,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array).then((val) => Promise.resolve(val));
 }
 
 /**
