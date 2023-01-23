e4f9a8cf6173f0e1daffa4df0527837fbc2879fc
squidawg <78506807+squidawg@users.noreply.github.com>
2023-01-23 23:20:59 +0400

    feat: start 09-functions-n-closures

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,11 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  // eslint-disable-next-line arrow-body-style
+  return (x) => {
+    return f(g(x));
+  };
 }
 
 
@@ -44,8 +47,11 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  // eslint-disable-next-line arrow-body-style
+  return (x) => {
+    return x ** exponent;
+  };
 }
 
 
@@ -62,8 +68,11 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  // eslint-disable-next-line arrow-body-style
+  return (x) => {
+    return [...args].reverse().map((a, i) => a * (x ** i)).reduce((a, b) => a + b, 0);
+  };
 }
 
 
